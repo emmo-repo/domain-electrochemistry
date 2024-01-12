144feb7e2499ac58e738d9f67fe2c6153be60344
Xavier Raynaud <xav.raynaud@gmail.com>
2024-01-12 16:09:21 +0100

    switched theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -116,19 +116,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-    }
+# html_theme_options = {
+#     # Toc options
+#     'collapse_navigation': False,
+#     'sticky_navigation': True,
+#     'navigation_depth': 4,
+#     'includehidden': True,
+#     'titles_only': False
+#     }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
