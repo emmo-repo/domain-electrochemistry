6ae872f51472b118b5d080455a8f85df54065e19
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-01-19 09:07:33 +0100

    clean-up in elisp scripts

--- sphinx/util.el
+++ sphinx/util.el
@@ -5,7 +5,7 @@
 
 (pcase (user-login-name)
   ("xavier" (progn
-              (pyvenv-activate "~/Python/battinfo-doc-3.7-env/")
+              (pyvenv-activate "~/Python/battinfo-doc-3.12-env/")
               (setq docdir "/home/xavier/Python/domain-electrochemistry/sphinx/")
               (setq testdir "/home/xavier/Python/BattInfo-doc-test/")
               ))
@@ -28,31 +28,5 @@
     (erase-buffer)
     (start-process "battinfo-build" outputbuffer "make" "html")
     )
-  )
-
-(defun battinfodoc-build-examples ()
-  "Run python publish script"
-  (interactive)
-  (let ((outputbuffer (get-buffer-create "*publishoutput*"))
-        (directory utilsdir))
-    (pop-to-buffer outputbuffer)
-    (erase-buffer)
-    (start-process "battinfo-publish" outputbuffer "python" (concat directory "buildPublishedExamples.py"))
-    )
-  )
-
-(defun battinfodoc-publish ()
-  "Copy and paste build directory in doc repo and push the result. Then open in a browser the action page in the repo and the published page"
-  (interactive)
-  (let ((default-directory docdir))
-    (shell-command (concat "cp -rf _build/html/* " testdir))
-    )
-  (let ((default-directory testdir))
-    (shell-command "git add *" )
-    (shell-command "git commit -m \"update in doc\"")
-    (shell-command "git push -f" )
-    )
-  (browse-url "https://github.com/BattMoTeam/BattInfo-doc-test/actions")
-  (browse-url "https://battmoteam.github.io/BattInfo-doc-test/")
   )
 
