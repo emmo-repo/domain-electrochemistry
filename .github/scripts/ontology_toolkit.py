fd402c89368346685d85c44671eee3e0e9bf90fd
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2025-03-17 19:10:29 +0100

    Change the documentation framework to use emmontopy

--- .github/scripts/ontology_toolkit.py
+++ .github/scripts/ontology_toolkit.py
@@ -10,6 +10,8 @@ from urllib.request import pathname2url
 from rdflib import Graph
 import logging
 from owlrl import DeductiveClosure, OWLRL_Semantics
+from ontopy.ontology import Ontology
+import owlready2
 
 def print_ttl_files():
     config = load_ontology_config()
@@ -109,21 +111,29 @@ def create_jsonld_context_file():
 ########## RST Documentation Generation (ttl_to_rst logic) ##########
 
 def load_ttl_from_file(filepath):
-    g = rdflib.Graph()
-    g.parse(filepath, format="turtle")
-    return g
+    from ontopy import get_ontology
+    onto = get_ontology(filepath).load()
+    return onto
+
+def _extract_all_annotations(value_list):
+    '''Help Function to extract both the text of a locstr and just str
+    from annotations. For now only choosing english. Note that this is a bit of a hack since not all annotations
+    are implemented as a locstr as they should. Perhaps this should be fixed in 
+    emmocheck instead?'''
+    result = []
+    for elem in value_list:
+        # For plain strings: only include if its type is exactly str.
+        if type(elem) is str:
+            result.append(elem)
+        # For locstr strings: include if its language is English.
+        elif hasattr(elem, "lang") and elem.lang == "en":
+            result.append(elem)
+    return result
 
 
-def extract_terms_info_sparql(g: Graph) -> list:
+def extract_terms_info_sparql(onto: Ontology) -> list:
     """Extracts terms from the TTL graph using SPARQL, including parent, subclass, and restriction links."""
     text_entities = []
-
-    PREFIXES = """
-        PREFIX emmo: <https://w3id.org/emmo#>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        """
 
     list_entity_types = [
         "IRI", "prefLabel", "Elucidation", "Alternative Label(s)",
@@ -133,89 +143,35 @@ def extract_terms_info_sparql(g: Graph) -> list:
         "Warning", "Admonition"
     ]
 
-    query = PREFIXES + """
-        SELECT ?iri ?prefLabel ?elucidation 
-               (GROUP_CONCAT(?altLabel; SEPARATOR=", ") AS ?altLabels) 
-               ?iecref ?iupacref ?wikipediaref ?wikidataref 
-               ?comment ?tip ?caution ?important ?note ?danger
-               ?error ?warning ?admonition
-        WHERE {
-            ?iri skos:prefLabel ?prefLabel.
+    config = load_ontology_config()
 
-            OPTIONAL { ?iri emmo:EMMO_967080e5_2f42_4eb2_a3a9_c58143e835f9 ?elucidation . }
-            OPTIONAL { ?iri skos:altLabel ?altLabel . }
-            OPTIONAL { ?iri emmo:EMMO_50c298c2_55a2_4068_b3ac_4e948c33181f ?iecref . }
-            OPTIONAL { ?iri emmo:EMMO_fe015383_afb3_44a6_ae86_043628697aa2 ?iupacref . }
-            OPTIONAL { ?iri emmo:EMMO_c84c6752_6d64_48cc_9500_e54a3c34898d ?wikipediaref . }
-            OPTIONAL { ?iri emmo:EMMO_26bf1bef_d192_4da6_b0eb_d2209698fb54 ?wikidataref . }
-            OPTIONAL { ?iri rdfs:comment ?comment . }
+    for entity in onto.get_entities(imported=False):
+        hit_dict = {'IRI': entity.iri}
+        # This is a bit of a hack, need to have a better way to be sure that we only 
+        # consider terms defined in the current ontology (and not just referenes here)
+        if not entity.iri.split('#')[0] == config["ontology_prefix"].rstrip('#/'):
+            continue
+ 
 
-            OPTIONAL { ?iri emmo:EMMO_b6730304_cabc_4104_b415_14218466445c ?caution . }
-            OPTIONAL { ?iri emmo:EMMO_5f7abc2a_5b50_41c3_8def_c8ba7c3b083e ?tip . }
-            OPTIONAL { ?iri emmo:EMMO_4c8480cf_56de_41da_8699_f12a9033313a ?important . }
-            OPTIONAL { ?iri skos:note ?note . }
-            OPTIONAL { ?iri emmo:EMMO_95317324_4b53_4665_a087_82c3a2a0540b ?danger . }
-            OPTIONAL { ?iri emmo:EMMO_32214c66_8bce_40f5_b22d_5059f6cf571b ?error . }
-            OPTIONAL { ?iri emmo:EMMO_0fa6566e_500f_4d24_baa9_b2b22c9a0b72 ?warning . }
-            OPTIONAL { ?iri emmo:EMMO_709ee08d_76b1_4fee_be22_925412ac313b ?admonition . }
+        #if not (isinstance(entity, owlready2.ThingClass) or isinstance(entity, owlready2.PropertyClass)):
+        #    continue
+
+        annotations = entity.get_annotations()
+        annotations_en = {
+                key: '; '.join(_extract_all_annotations(item))
+            for key, item in annotations.items()
         }
-        GROUP BY ?iri ?prefLabel ?elucidation ?caution ?tip ?important ?note ?danger ?error ?warning ?admonition
-        """
 
-    qres = g.query(query)
+        hit_dict.update(annotations_en)
 
-    for hit in qres:
-        hit_dict = {entity_type: str(entity) for entity_type, entity in zip(list_entity_types, hit)}
-
-        # Fetch direct parents (rdfs:subClassOf)
-        parents = []
-        parent_query = PREFIXES + """
-            SELECT ?parent ?parentLabel WHERE {
-                <%s> rdfs:subClassOf ?parent .
-                ?parent skos:prefLabel ?parentLabel .
-            }
-        """ % hit_dict['IRI']
-        for row in g.query(parent_query):
-            parent_iri, parent_label = row
-            parents.append((str(parent_iri), str(parent_label)))
+        parents = [ent for ent in entity.is_a if (isinstance(ent, owlready2.ThingClass) or isinstance(ent, owlready2.PropertyClass))]
         hit_dict["Parent Classes"] = parents
 
         # Fetch direct subclasses
-        subclasses = []
-        subclass_query = PREFIXES + """
-            SELECT ?subclass ?subclassLabel WHERE {
-                ?subclass rdfs:subClassOf <%s> .
-                ?subclass skos:prefLabel ?subclassLabel .
-            }
-        """ % hit_dict['IRI']
-        for row in g.query(subclass_query):
-            subclass_iri, subclass_label = row
-            subclasses.append((str(subclass_iri), str(subclass_label)))
-        hit_dict["Subclasses"] = subclasses
+        subclasses = list(entity.subclasses())
 
         # Fetch OWL Restrictions (object property + someValuesFrom)
-        restrictions = []
-        restriction_query = PREFIXES + """
-            SELECT ?prop ?propLabel ?target ?targetLabel WHERE {
-                <%s> rdfs:subClassOf [
-                    rdf:type owl:Restriction ;
-                    owl:onProperty ?prop ;
-                    owl:someValuesFrom ?target
-                ] .
-                ?prop skos:prefLabel ?propLabel .
-                ?target skos:prefLabel ?targetLabel .
-            }
-        """ % hit_dict['IRI']
-
-        for row in g.query(restriction_query):
-            prop_iri, prop_label, target_iri, target_label = map(str, row)
-            restrictions.append({
-                "property_iri": prop_iri,
-                "property_label": prop_label,
-                "target_iri": target_iri,
-                "target_label": target_label,
-            })
-
+        restrictions = [restriction for restriction in entity.is_a if isinstance(restriction, owlready2.Restriction)]
         hit_dict["Restrictions"] = restrictions
 
         text_entities.append(hit_dict)
@@ -271,7 +227,6 @@ def entities_to_rst(entities: list[dict]) -> str:
 
         rst += ".. raw:: html\n\n"
         rst += f"   <div id=\"{iri_suffix}\"></div>\n\n"
-        
         rst += f"{item['prefLabel']}\n"
         rst += "-" * len(item['prefLabel']) + "\n\n"
         rst += f"IRI: {item['IRI']}\n\n"
@@ -288,18 +243,26 @@ def entities_to_rst(entities: list[dict]) -> str:
             rst += "  <tr>\n"
             rst += f"    <td class=\"element-table-key\"><span class=\"element-table-key\">{key}</span></td>\n"
 
+            #print(value)
             if value.startswith("http"):
                 value = f"<a href='{value}'>{value}</a>"
 
             rst += f"    <td class=\"element-table-value\">{value}</td>\n"
             rst += "  </tr>\n"
-
         # Add parent classes section
         if item.get("Parent Classes"):
             rst += "  <tr>\n"
             rst += "    <td class=\"element-table-key\"><span class=\"element-table-key\">Parent Classes</span></td>\n"
             rst += "    <td class=\"element-table-value\">"
-            parent_links = [f"<a href='#{iri.split('#')[-1]}'>{label}</a>" for iri, label in item["Parent Classes"]]
+
+            parent_links = []
+            for parent in item["Parent Classes"]:
+                try:
+                    parent_links.append(f"<a href='#{parent.iri.split('#')[-1]}'>{parent.prefLabel.get_lang('en')[0]}</a>")
+                except:
+                    parent_links.append(f"<a href='#{parent.iri.split('#')[-1]}'>{parent}</a>")
+
+            
             rst += ", ".join(parent_links)
             rst += "</td>\n"
             rst += "  </tr>\n"
@@ -319,15 +282,18 @@ def entities_to_rst(entities: list[dict]) -> str:
             # Group restrictions by property_label for cleaner table (optional, but nice)
             grouped_restrictions = {}
             for restriction in item["Restrictions"]:
-                prop_label = restriction['property_label']
-                target_link = f"<a href='#{restriction['target_iri'].split('#')[-1]}'>{restriction['target_label']}</a>"
-                if prop_label not in grouped_restrictions:
-                    grouped_restrictions[prop_label] = []
-                grouped_restrictions[prop_label].append(target_link)
+                try:
+                    target_link = f"<a href='#{restriction.value.iri.split('#')[-1]}'>{restriction.value}</a>"
+                except AttributeError:
+                    target_link = f"<a href='#{restriction.value}'>{restriction.value}</a>"
+
+                if restriction.property not in grouped_restrictions:
+                    grouped_restrictions[restriction.property] = []
+                grouped_restrictions[restriction.property].append(target_link)
 
             for prop_label, target_links in grouped_restrictions.items():
                 rst += "  <tr>\n"
-                rst += f"    <td class=\"element-table-key\"><span class=\"element-table-key\">{prop_label}</span></td>\n"
+                rst += f"    <td class=\"element-table-key\"><span class=\"element-table-key\">{restriction.property}</span></td>\n"
                 rst += "    <td class=\"element-table-value\">"
                 rst += ", ".join(target_links)
                 rst += "</td>\n"
@@ -374,8 +340,8 @@ def generate_rst_documentation():
     for module in config["ttl_files"]:
         filepath = module["path"]
         if os.path.isfile(filepath):
-            g = load_ttl_from_file(filepath)
-            entities = extract_terms_info_sparql(g)
+            onto = load_ttl_from_file(filepath)
+            entities = extract_terms_info_sparql(onto)
             rst_content += f"\n{module['section_title']}\n{'=' * len(module['section_title'])}\n\n"
             rst_content += entities_to_rst(entities)
 
@@ -479,6 +445,9 @@ def run_reasoner_check():
         sys.exit(1)
 
 ########## Main Entry Point ##########
+
+#def run_ontodoc():
+
 
 def main():
     config = load_ontology_config()
